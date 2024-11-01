542b8c211f3442bb7405e7c41c7a985eb2158330
prideandjuice <elvirarahma07@gmail.com>
2024-11-01 16:28:21 +0700

    first commit

---
+++ mochi.js
@@ -0,0 +1,6 @@
+function openModal() {
+    document.getElementById("orderModal").style.display = "flex";
+}
+function closeModal() {
+    document.getElementById("orderModal").style.display = "none";
+}
